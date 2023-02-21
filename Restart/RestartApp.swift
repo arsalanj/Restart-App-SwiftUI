a0744621a641b9de516789f49512352149b5a4ed
Muhammad Mehdi <mehdi@spiretech.co>
2023-02-21 15:48:16 +0500

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Mehdi on 21/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
